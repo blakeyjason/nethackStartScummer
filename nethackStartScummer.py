1d6ee972d4b3c5da2db9a750497ee72b73882f55
Jason Blakey <jblakey@gmail.com>
2020-02-06 13:48:45 -0500

    Added the script to repo

---
+++ nethackStartScummer.py
@@ -0,0 +1,80 @@
+import pexpect, sys, time
+
+
+# Make sure these items are in the order that they normally appear
+# in your inventory...
+# The default order is Scrolls, Spellbooks, Potions, Rings, Wands, Tools
+# DO NOT USE META CHARACTERS HERE...
+wanted_objects = ["identify", "spellbook of charm", "smoky", "magic marker"]
+
+
+def scum(wantedObjectsList):
+
+    myWantedObjects = []
+
+    # First, build the regex from the wantedObjectsList...
+    # This is a little junky, and prone to breakage...
+    big_regex = ""
+    for thisItem in wantedObjectsList:
+         big_regex += "(" + thisItem + ").+"
+
+    myWantedObjects.append(big_regex)
+    print(myWantedObjects)
+    
+    # And then append the (end) tag...
+    myWantedObjects.append(r'\(end\)')
+
+    # Add this in case we get that darn blindfold, and it kicks
+    # us over to a 2 page inventory...
+    myWantedObjects.append(r'\(1 of 2\)')
+
+    n = pexpect.spawn('nethack')
+    n.setecho(True)
+    r = n.expect("Shall I pick")
+    n.send("nwhmny")
+    n.expect("Go bravely")
+    n.send(' ')
+    n.expect("welcome to")
+    n.sendline("")
+
+    # Send a few spaces in case we're standing on anything...
+    n.send(' ')
+    n.send(' ')
+    n.send(' ')
+
+    # Send the inventory command...
+    n.send("i")
+
+    match_index  = n.expect(myWantedObjects, timeout=1)
+
+    n.send(' ')
+    n.send(' ')
+    n.send(' ')
+
+    # Success, save the character...
+    if match_index == 0:
+        n.sendline("Sy")
+        return True
+
+    # Otherwise, toss this character...
+    else:
+        n.sendline("#quit")
+        n.send('y')
+        n.send('q')
+        return False
+
+def main():
+
+    for i in range(1, 1000):
+
+        print("Run: " + str(i))
+        success = scum(wanted_objects)
+
+        if success:
+            print("Your character has all the items you wanted, is saved and ready to go!")
+            exit()
+        else:
+            continue
+
+if __name__ == "__main__":
+    main()
